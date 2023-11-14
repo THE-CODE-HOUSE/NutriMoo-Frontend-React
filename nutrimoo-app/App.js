28671acdfaeab5023476ee1be805edbb5178628b
Marcos <marcosvaleriofilho@outlook.com>
2023-11-13 23:00:00 -0300

    Tela Animal Info!

--- nutrimoo-app/App.js
+++ nutrimoo-app/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { View } from 'react-native';
 import ListScreen from './src/screens/ListScreen';
+import AnimalInfo from './src/screens/Animal_Info';
 
 
 export default function App() {
   return (
     <View style={{ flex: 1}}>
-      <ListScreen />
+      <AnimalInfo />
     </View>
   );
 }
