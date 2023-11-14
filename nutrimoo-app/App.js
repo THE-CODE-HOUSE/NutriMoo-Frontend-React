c012df53bbe178a073adbe773cc22ae4aea9a2c7
fermiwa <104083221+fermiwa@users.noreply.github.com>
2023-11-14 09:09:09 -0300

    Tela Select Animal Type criada

--- nutrimoo-app/App.js
+++ nutrimoo-app/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { View } from 'react-native';
 import SignInScreen from './src/screens/SignIn';
+import SelectAnimalType from './src/screens/SelectAnimalType';
 
 
 export default function App() {
   return (
     <View style={{ flex: 1}}>
-      <SignInScreen />
+      <SelectAnimalType />
     </View>
   );
 }
---
+++ nutrimoo-app/src/screens/SelectAnimalType/styles.js
@@ -0,0 +1,87 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  titletext: {
+    fontSize: 40,
+    color: "#073021",
+    fontWeight: "bold",
+    marginTop: "5%",
+  },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-start",
+    width: "100%",
+    resizeMode: "cover",
+  },
+  retangulo: {
+    position: "absolute",
+    bottom: 1,
+    width: "100%",
+    height: "84%",
+    backgroundColor: "#fffff",
+    alignItems: "center",
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+  },
+  shadowBox: {
+    width: 350,
+    height: 50,
+    marginTop: 10,
+    borderRadius: 20,
+    backgroundColor: "white",
+    elevation: 15,
+  },
+  homeBar: {
+    //backgroundColor: "white",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    height: "10%",
+  },
+  button: {
+    width: 160,
+    height: 130,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: "#9BFF99",
+    borderRadius: 30,
+    elevation: 5,
+    marginTop: 60,
+    margin:10
+  },
+  /*iconContainer: {
+    width:"01%",
+    height:"50%",
+    backgroundColor: "#9BFF99", // Cor de fundo do ícone
+    borderRadius: 20, // Ajuste para corresponder ao design
+    padding: 5,
+    marginTop: 10,
+    alignContent: "center"
+  },*/
+  icon: {
+    color: "#073021",
+    width: "100%", // Ajuste conforme necessário
+    height: "100%", // Ajuste conforme necessário
+  },
+  textContainer: {
+    borderRadius: 30,
+    height:"100%",
+    width:"100%",
+    alignContent: "center",
+    opacity: 0.7,
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 20,
+    color: "#073021",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  teste:{
+    flexDirection:"row",
+  }
+});
+
+export default styles;
